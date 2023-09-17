447f77f934a5149caa08ade0571b64b9eefe5362
Panos Antonellos <pantonel94@gmail.com>
2023-09-17 17:37:17 +0300

    merge fixes

--- src/config/index.ts
+++ src/config/index.ts
@@ -11,7 +11,7 @@ export const Access_Token_Secret = process.env.ACCESS_TOKEN_SECRET!;
 export const Refresh_Token_Secret = process.env.REFRESH_TOKEN_SECRET!;
 export const Client_URL = process.env.CLIENT_URL;
 
-export const port = process.env.PORT;
+export const port = process.env.PORT || 3000;
 export const DB_Path = `mongodb+srv://${DB_Username}:${DB_Password}@${DB_Url}/${DB_dbname}`;
 
 export default { Client_URL, port, DB_Path, Access_Token_Secret, Refresh_Token_Secret };
