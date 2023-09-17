b0692cdd9f7b29f7ccffd0ff6e8f04ff6191a699
Panos Antonellos <pantonel94@gmail.com>
2023-09-17 17:02:59 +0300

    change project configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,7 +7,7 @@ const DB_Password = process.env.DB_PASSWORD;
 const DB_Url = process.env.DB_URL;
 const DB_dbname = process.env.DB_DBNAME;
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const DB_Path = `mongodb+srv://${DB_Username}:${DB_Password}@${DB_Url}/${DB_dbname}`;
 
 export default { port, DB_Path };
